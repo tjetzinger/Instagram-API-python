62b68f6c10e43d128a7937948c756f1631495d61
Moacir Moda <moa.moda@gmail.com>
2016-10-23 21:32:31 -0200

    fixing the url from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='instagram-api',
       version='0.1',
       description='Unofficial instagram API, give you access to ALL instagram features (like, follow, upload photo and video and etc)! Write on python.',
-      url='https://github.com/moacirmoda/pygram',
+      url='https://github.com/LevPasha/Instagram-API-python/',
       author='Lions Crayons',
       author_email='info@lionscrayons.com',
       license='GNU',
